1d72ee19b8954b2c0c04c203425e93aa02f701d9
SteelTitansax <manuel.portero.leiva@gmail.com>
2025-03-01 15:19:55 +0100

    code refactoring home.html

--- professional_portfolio/portfolio_webapp/views.py
+++ professional_portfolio/portfolio_webapp/views.py
@@ -9,6 +9,11 @@ def home(request):
     sections = {
         "portfolios": load_json("portfolios"),
         "curriculum": load_json("curriculum"),
-        "education": load_json("education")
+        "education": load_json("education"),
+        "experiences": load_json("experiences"),
+        "hobbies": load_json("hobbies"),
+        "skills": load_json("skills")
     }
+
+    print(sections["hobbies"])
     return render(request, "home.html", {"sections": sections})
