1839b2ba6b54f95fca8d98ff3b9cc86422aa6bbb
SteelTitansax <manuel.portero.leiva@gmail.com>
2025-03-01 20:23:46 +0100

    documents modal

--- professional_portfolio/portfolio_webapp/static/js/script.js
+++ professional_portfolio/portfolio_webapp/static/js/script.js
@@ -4,3 +4,52 @@ document.addEventListener('DOMContentLoaded', function () {
         return new bootstrap.Tooltip(tooltipTriggerEl);
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+
+    document.querySelectorAll(".curriculum-icon").forEach(function (element) {
+        element.addEventListener("click", function () {
+            let pdfUrl = this.getAttribute("data-pdf");
+            document.getElementById("pdfViewer").src = pdfUrl;
+            let pdfModal = new bootstrap.Modal(document.getElementById("pdfModal"));
+            pdfModal.show();
+        });
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+
+    document.querySelectorAll(".education-icon").forEach(function (element) {
+        element.addEventListener("click", function () {
+            let pdfUrl = this.getAttribute("data-pdf");
+            document.getElementById("pdfViewer").src = pdfUrl;
+            let pdfModal = new bootstrap.Modal(document.getElementById("pdfModal"));
+            pdfModal.show();
+        });
+    });
+    document.addEventListener("DOMContentLoaded", function () {
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+        tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+            new bootstrap.Tooltip(tooltipTriggerEl);
+        });
+    
+        document.querySelectorAll(".curriculum-icon").forEach(function (element) {
+            element.addEventListener("click", function () {
+                let pdfUrl = this.getAttribute("data-pdf");
+                document.getElementById("pdfViewer").src = pdfUrl;
+                let pdfModal = new bootstrap.Modal(document.getElementById("pdfModal"));
+                pdfModal.show();
+            });
+        });
+    });
+});
