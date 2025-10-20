f31ed03205fb4627410352e38ae79554b3083519
SteelTitansax <112019917+SteelTitansax@users.noreply.github.com>
2025-10-20 16:49:22 +0200

    Add comment for static files preparation before deploy
    Added a comment to remind users to prepare static files before deployment.

--- professional_portfolio/deploy.sh
+++ professional_portfolio/deploy.sh
@@ -1,2 +1,3 @@
 az webapp up --name manuel-portero-professional-website --resource-group rg-spaincentral-portfolio-mpl --runtime "PYTHON:3.9"
+# Use this comand before deploy the app, for preparing the static files. 
 # python manage.py collectstatic
