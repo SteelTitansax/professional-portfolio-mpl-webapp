7b53bd9c45c61d666717620ad0f2302073c364f8
SteelTitansax <112019917+SteelTitansax@users.noreply.github.com>
2025-10-20 17:06:28 +0200

    Add Gunicorn startup command to deploy script
    Added startup command for Gunicorn in deploy script.

--- professional_portfolio/deploy.sh
+++ professional_portfolio/deploy.sh
@@ -1,3 +1,5 @@
 az webapp up --name manuel-portero-professional-website --resource-group rg-spaincentral-portfolio-mpl --runtime "PYTHON:3.9"
+# startup command
+# gunicorn --workers 3 --bind 0.0.0.0:8000 professional_portfolio.wsgi:application
 # Use this comand before deploy the app, for preparing the static files. 
 # python manage.py collectstatic
